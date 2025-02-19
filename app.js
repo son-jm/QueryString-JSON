fcf0190c2850083ec95deea587183472c971165d
=son-jm <=>
2025-02-19 10:18:29 +0900

    문제2번

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// // 1
+// const data = {
+//   name: "이영희",
+//   age: 30,
+//   job: "개발자",
+// };
+// const da = qs.stringify(data);
+// console.log(da);
+
+// 2
 const data = {
-  name: "이영희",
-  age: 30,
-  job: "개발자",
+  title: "Node.js Study",
+  duration: 3,
+  completed: false,
 };
-const da = qs.stringify(data);
+
+const da = JSON.stringify(data);
 console.log(da);
