bef166b5f74cafc8081c105d6cf0ae5f16d3b243
=son-jm <=>
2025-02-19 14:48:07 +0900

    서버 시작

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const a = {
-  user: {
-    name: "홍길동",
-    age: 25,
-  },
-};
-const da = qs.stringify(data);
-console.log(da);
-const aa = JSON.stringify(data);
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    if (request.url === "/") {
+      const MAIN_PAGE = fs.readFileSync("index.html");
+      response.writeHead(200, { "content-type": "utf-8; text/html" });
+      response.end(MAIN_PAGE);
+    }
+  }
+});
+
+server.listen(3000, function () {
+  console.log("서버 도는중 http://localhost:3000");
+});
