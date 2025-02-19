6fa3e6e8ed3f8e92af7b6e046f33324c16d84541
=son-jm <=>
2025-02-19 14:55:17 +0900

    app.js에 css 설정

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const server = http.createServer(function (request, response) {
       const MAIN_PAGE = fs.readFileSync("index.html");
       response.writeHead(200, { "content-type": "utf-8; text/html" });
       response.end(MAIN_PAGE);
+    } else if (request.url === "/style.css") {
+      const style = fs.readFileSync("style.css", "utf-8");
+
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/css");
+      response.end(style);
     }
   }
 });
